9e31cbcae54f40151677acc23d0ebea6040ff6dc
Lari Lohikoski <lari.lohikoski@iki.fi>
2023-01-20 18:35:54 +0000

    Enable 5000 char toots!

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -11,7 +11,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :domain, :title, :version, :source_url, :description,
              :usage, :thumbnail, :languages, :configuration,
-             :registrations
+             :registrations :max_toot_chars
 
   has_one :contact, serializer: ContactSerializer
   has_many :rules, serializer: REST::RuleSerializer
@@ -87,6 +87,10 @@ class REST::InstanceSerializer < ActiveModel::Serializer
     }
   end
 
+def max_toot_chars 
+    5000 
+ end
+ 
   private
 
   def registrations_enabled?
